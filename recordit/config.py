962f1fe56f78d1f6ae79213fba86abc8f51f808b
Hui Yiqun <huiyiqun@gmail.com>
2017-04-17 18:20:33 +0800

    replace cerberus with schema for cerberus is hard to use

--- recordit/config.py
+++ recordit/config.py
@@ -1,19 +1,15 @@
 import os
 
-from cerberus import Validator
+from sqlalchemy import create_engine
+from schema import Schema, Use, Optional
 
 
 class Config:
     def __init__(self):
-        schema = {
-            'DB': {
-                'type': 'string',
-                'default': f'sqlite:///{os.getcwd()}/recordit.db',
-            },
-        }
-        v = Validator(schema, purge_unknown=True)
-
-        self._cfg = v.normalized(os.environ)
+        schema = Schema({
+            Optional('DB', default=f'sqlite:///{os.getcwd()}/recordit.db'): Use(create_engine)
+        }, ignore_extra_keys=True)
+        self._cfg = schema.validate(dict(os.environ))
 
     def __getitem__(self, x):
         return self._cfg[x]
